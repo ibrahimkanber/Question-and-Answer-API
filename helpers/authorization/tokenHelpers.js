535a2809f2ecb028e6a5bf292e04cb3d86c07eca
ibrahimkanber <65809527+ibrahimkanber@users.noreply.github.com>
2021-05-04 22:45:42 +0200

    Update tokenHelpers.js

--- helpers/authorization/tokenHelpers.js
+++ helpers/authorization/tokenHelpers.js
@@ -9,7 +9,7 @@ const sendJwtToClient=(user,response)=>{
     .cookie("access_token",token,{
         httpOnly:true,
         exprires:new Date(Date.now()+parseInt(JWT_COOKIE)*1000*60),
-        secure:NODE_ENV==="development" ? false:true
+        secure:false
     })
     .json({
         success:true,
